d88c463d80d9955aceeadc478b708ec9234c233c
Jonas3221 <83805516+Jonas3221@users.noreply.github.com>
2021-12-10 15:23:06 +0100

    Besked bekræftelse
    kontakt

---
+++ hjemmeside/JS/kontakt.js
@@ -0,0 +1,20 @@
+
+var bekræft = document.getElementById("bekræft");
+
+var btn = document.getElementById("knap");
+
+var luk = document.getElementsByClassName("luk")[0];
+
+btn.onclick = function() {
+  bekræft.style.display = "block";
+}
+
+luk.onclick = function() {
+  bekræft.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == bekræft) {
+    bekræft.style.display = "none";
+  }
+}
